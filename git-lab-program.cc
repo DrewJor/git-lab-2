cab896d0ee7ac26a27588427d97bb92bbee3bb9f
Drew Jordan <jjordan.andrewj@gmail.com>
2023-01-24 20:07:05 -0500

    Enter a message here

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Drew Jordan
+ *        Date: January 24th 2023
+ * Description: hi
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
